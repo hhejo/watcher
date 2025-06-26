4efd9302bdac46d71ab8d66e293d1fbe59076039
hhejo <litsmainjof@gmail.com>
2025-06-26 20:57:39 +0900

    refactor: config 파일 추가

---
+++ config.py
@@ -0,0 +1,7 @@
+# WATCH_PATH = r'\\192.168.1.231\m24h-p01gm_gm_2ppm_pkg\GM 공용 폴더'
+# WATCH_PATH = r'C:\Users\hj\Documents\000_개인\hjpy\test'
+WATCH_PATH = r'./test'
+SLEEP_TIME = 5
+DEBOUNCE = 5
+IGNORE_PATTERNS = ['~$*', '*@SynoEAStream', '*@SynoResource', '@eaDir/*']
+ALARM_TYPES = {'created': '생성', 'modified': '수정', 'moved': '이동', 'deleted': '삭제'}
--- main.py
+++ main.py
@@ -2,18 +2,11 @@ import time
 from pathlib import PureWindowsPath
 from datetime import datetime
 from typing import Dict, Tuple
-# from win10toast import ToastNotifier
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+# from win10toast import ToastNotifier
 
-# WATCH_PATH = r'\\192.168.1.231\m24h-p01gm_gm_2ppm_pkg\GM 공용 폴더'
-# WATCH_PATH = r'C:\Users\hj\Documents\000_개인\hjpy\test'
-WATCH_PATH = r'./test'
-SLEEP_TIME = 5
-DEBOUNCE = 5
-IGNORE_PATTERNS = ['~$*', '*@SynoEAStream', '*@SynoResource', '@eaDir/*']
-ALARM_TYPES = {'created': '생성', 'modified': '수정', 'moved': '이동', 'deleted': '삭제'}
-
+from config import *
 
 # toaster = ToastNotifier()
 
