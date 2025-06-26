d6cf1732cfe23e3551ab3cb25e5e9a7489153bc0
hhejo <litsmainjof@gmail.com>
2025-06-26 21:00:16 +0900

    feat: 운영체제에 따라 경로 설정

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
-# WATCH_PATH = r'\\192.168.1.231\m24h-p01gm_gm_2ppm_pkg\GM 공용 폴더'
-# WATCH_PATH = r'C:\Users\hj\Documents\000_개인\hjpy\test'
-WATCH_PATH = r'./test'
+import platform
+
+if platform.system() == "Windows":
+    WATCH_PATH = r'\\192.168.1.231\m24h-p01gm_gm_2ppm_pkg\GM 공용 폴더'
+else:
+    WATCH_PATH = r'./test'
+
 SLEEP_TIME = 5
 DEBOUNCE = 5
 IGNORE_PATTERNS = ['~$*', '*@SynoEAStream', '*@SynoResource', '@eaDir/*']
